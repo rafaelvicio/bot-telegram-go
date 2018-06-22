3c98fb0becac951ca5ecc2684047c2b9e08d2d46
Rafael Vicio <rafaelvicio@icloud.com>
2018-06-22 00:36:55 -0300

    Create Bot

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+
+	"gopkg.in/telegram-bot-api.v4"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI("441362777:AAEfJT-cNrTvRzkxGhJWbrWjftb-NJyVWK0")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		if update.Message.IsCommand() {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+			switch update.Message.Command() {
+			case "help":
+				msg.Text = "I help you"
+			}
+			bot.Send(msg)
+		}
+
+	}
+}
